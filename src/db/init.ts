b93985947217e810f09ecf34a0d03c0062bd7c51
auntjoestar <88282065+Auntjoestar@users.noreply.github.com>
2025-08-14 01:25:03 -0600

    configure turso db

--- src/db/init.ts
+++ src/db/init.ts
@@ -1,45 +1,12 @@
-import { drizzle } from "drizzle-orm/bun-sqlite";
+import { drizzle } from "drizzle-orm/libsql";
 
 export function initDB() {
-  const db = drizzle(":memory:");
-
-  db.run(`
-  CREATE TABLE IF NOT EXISTS characters_table (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL,
-    alias TEXT NOT NULL,
-    category TEXT NOT NULL,
-    weapon TEXT NOT NULL
-  );
-`);
-
-  db.run(`
-  CREATE TABLE IF NOT EXISTS abilities_table (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL,
-    description TEXT NOT NULL,
-    duration INTEGER NOT NULL,
-    cooldown INTEGER NOT NULL,
-    character_id INTEGER NOT NULL,
-    FOREIGN KEY (character_id) REFERENCES characters_table(id)
-  );
-`);
+  const db = drizzle({
+    connection: {
+      url: process.env.TURSO_DATABASE_URL!,
+      authToken: process.env.TURSO_AUTH_TOKEN!,
+    },
+  });
 
   return db;
 }
-
-//
-// export function initDB() {
-//
-// const db = drizzle({
-//   connection: {
-//     url: process.env.TURSO_DATABASE_URL!,
-//     authToken: process.env.TURSO_AUTH_TOKEN!,
-//   },
-// });
-//
-//
-//
-//   return db;
-// }
-//
